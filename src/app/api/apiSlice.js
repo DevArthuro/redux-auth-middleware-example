979988bdf1657ab971899789543b4a8c2701be68
DevArthuro <caorrego38@soy.sena.edu.co>
2024-06-08 22:35:07 -0500

    feat: refresh token endpoint

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -12,3 +12,27 @@ const baseQuery = fetchBaseQuery({
     return headers;
   },
 });
+
+const baseQueryRefreshToken = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result.error.originalStatus === 403) {
+    console.log("Send refresh token");
+    const refreshResult = await baseQuery("/refresh", api, extraOptions);
+    console.log("Refreshing rsults");
+    if (refreshResult.data) {
+      const user = api.getState().auth.user;
+      api.dispatch(loginUser({ ...refreshResult.data, user }));
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logoutUser());
+    }
+  }
+
+  return result;
+};
+
+export const apiSlice = createApi({
+  baseQuery: baseQueryRefreshToken,
+  endpoints: (builder) => ({}),
+});

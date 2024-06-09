633f9fec44170987a26de3fa6a9819a78ec342fe
DevArthuro <caorrego38@soy.sena.edu.co>
2024-06-08 23:04:14 -0500

    feat: add react router

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,16 @@ import App from "./App.jsx";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );

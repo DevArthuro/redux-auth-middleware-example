51c3a7cd9e9562839f2f863c6c10cef8aa77fd69
DevArthuro <caorrego38@soy.sena.edu.co>
2024-06-10 14:50:21 -0500

    feat: add functionality login

--- src/features/auth/login.jsx
+++ src/features/auth/login.jsx
@@ -1,4 +1,56 @@
+import { useRef, useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+import { loginUser } from "./authSlice";
+import { useLoginMutation } from "./authApiSlice";
+
 const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMessage, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setMessage("");
+  }, [user, password]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const userData = await login({ user, password }).unwrap();
+      dispatch(loginUser({ ...userData, user }));
+      setUser("");
+      setPassword("");
+      navigate("/welcome");
+    } catch (error) {
+      if (!err?.originalStatus) {
+        errMessage("No Server Response");
+      } else if (err.originalStatus === 400) {
+        errMessage("Missing Username or Password");
+      } else if (err.originalStatus === 401) {
+        errMessage("Unauthorized");
+      } else {
+        errMessage("Login Failed");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  const handleUserInput = (e) => setUser(e.target.value);
+
+  const handlePasswordInput = (e) => setPassword(e.target.value);
+
   return <div>Login</div>;
 };
 

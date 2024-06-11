1d9558ed57085c2f4e5b3c119f10eb5679e6e437
DevArthuro <caorrego38@soy.sena.edu.co>
2024-06-10 23:47:08 -0500

    feat: connects routes and last details

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout";
+import Public from "./components/public";
+import Login from "./features/auth/login";
+import Welcome from "./features/auth/welcome";
+import RequiredAuth from "./features/auth/requiredAuth";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public routes */}
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+        {/* Protected routes */}
+        <Route element={<RequiredAuth />}>
+          <Route path="welcome" element={<Welcome />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
